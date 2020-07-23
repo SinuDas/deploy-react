ba53dbfe2be42d466a8e5f34c25c2b2c2cb7b4ea
SinuDas <sinudas08@gmail.com>
2020-07-23 11:42:45 +0530

    Updates

---
+++ precache-manifest.f10fb3ae16cc0bafdd42acf1668d7cef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d868eece2f41e03a9422103d44c5839a",
+    "url": "/SinuDas.github.io/deploy-react/index.html"
+  },
+  {
+    "revision": "1b1d14aa573ebdbdeb53",
+    "url": "/SinuDas.github.io/deploy-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ecf4686a5c9e7b7fc122",
+    "url": "/SinuDas.github.io/deploy-react/static/css/main.3b20f63f.chunk.css"
+  },
+  {
+    "revision": "1b1d14aa573ebdbdeb53",
+    "url": "/SinuDas.github.io/deploy-react/static/js/2.b804388d.chunk.js"
+  },
+  {
+    "revision": "a8f962b213a67202a0ddc78ab1211cdb",
+    "url": "/SinuDas.github.io/deploy-react/static/js/2.b804388d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecf4686a5c9e7b7fc122",
+    "url": "/SinuDas.github.io/deploy-react/static/js/main.307da082.chunk.js"
+  },
+  {
+    "revision": "b9caaefb7c1a31c1a660",
+    "url": "/SinuDas.github.io/deploy-react/static/js/runtime-main.f507e7bf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sinudas.github.io/deploy-react/precache-manifest.bfcbdc0258349a0d6269e104ff3f7d41.js"
+  "/SinuDas.github.io/deploy-react/precache-manifest.f10fb3ae16cc0bafdd42acf1668d7cef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sinudas.github.io/deploy-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SinuDas.github.io/deploy-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
