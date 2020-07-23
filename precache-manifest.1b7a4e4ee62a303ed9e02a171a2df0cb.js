b63d19c38c3494b957c604da128bd6d20860d619
SinuDas <sinudas08@gmail.com>
2020-07-23 12:07:28 +0530

    Updates

---
+++ precache-manifest.1b7a4e4ee62a303ed9e02a171a2df0cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf6a82e74725ce7b48dff3c38ef8d0ab",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "1b1d14aa573ebdbdeb53",
+    "url": "/deploy-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "927da72a5fba0cad5536",
+    "url": "/deploy-react/static/css/main.3b20f63f.chunk.css"
+  },
+  {
+    "revision": "1b1d14aa573ebdbdeb53",
+    "url": "/deploy-react/static/js/2.b804388d.chunk.js"
+  },
+  {
+    "revision": "a8f962b213a67202a0ddc78ab1211cdb",
+    "url": "/deploy-react/static/js/2.b804388d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "927da72a5fba0cad5536",
+    "url": "/deploy-react/static/js/main.864c6538.chunk.js"
+  },
+  {
+    "revision": "68daa4b9dce428ddc429",
+    "url": "/deploy-react/static/js/runtime-main.06cf774c.js"
+  }
+]);
