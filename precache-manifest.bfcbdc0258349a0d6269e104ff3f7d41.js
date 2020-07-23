92d5045f1fa05393f73954370f537890a3f380cb
SinuDas <sinudas08@gmail.com>
2020-07-23 11:57:58 +0530

    Updates

---
+++ precache-manifest.bfcbdc0258349a0d6269e104ff3f7d41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51dfb1e7190f9216046fafd08cdf040a",
+    "url": "/sinudas.github.io/deploy-react/index.html"
+  },
+  {
+    "revision": "1b1d14aa573ebdbdeb53",
+    "url": "/sinudas.github.io/deploy-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e7b35d8a3a9172ff9b6f",
+    "url": "/sinudas.github.io/deploy-react/static/css/main.3b20f63f.chunk.css"
+  },
+  {
+    "revision": "1b1d14aa573ebdbdeb53",
+    "url": "/sinudas.github.io/deploy-react/static/js/2.b804388d.chunk.js"
+  },
+  {
+    "revision": "a8f962b213a67202a0ddc78ab1211cdb",
+    "url": "/sinudas.github.io/deploy-react/static/js/2.b804388d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b35d8a3a9172ff9b6f",
+    "url": "/sinudas.github.io/deploy-react/static/js/main.6e07a76e.chunk.js"
+  },
+  {
+    "revision": "1caedf39210efcc78076",
+    "url": "/sinudas.github.io/deploy-react/static/js/runtime-main.c6c753a6.js"
+  }
+]);
